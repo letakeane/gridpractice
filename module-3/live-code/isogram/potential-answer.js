b2c87c0898a8ad245b703ea9c373b364ee6791b2
joshuajhun <joshuajhun@gmail.com>
2018-03-15 14:51:30 -0600

    added live code answers for isogram

---
+++ module-3/live-code/isogram/potential-answer.js
@@ -0,0 +1,32 @@
+export default class Isogram {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase()
+  }
+
+  cleanPhrase(phrase) {
+    const nonValid = ['-', ' ']
+    return phrase.filter(char => nonValid.indexOf(char) > 0)
+  };
+
+  determainIso(array, key= {}, i = 0) {
+    const currentLetter = array[i]
+
+    key[currentLetter] = (key[currentLetter] || 0) + 1;
+
+    const notIso    = key[currentLetter] > 1
+    const isFinshed = i >= array.length  - 1
+
+     if(notIso) {
+       return false
+     }
+
+     if(!isFinshed) {
+       return this.determainIso(array, key, i + 1)
+     }
+     return true
+  }
+
+  isIsogram() {
+    return this.determainIso(this.cleanPhrase(this.phrase.split('')))
+  }
+}
